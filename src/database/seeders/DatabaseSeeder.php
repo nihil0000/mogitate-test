3722d7d4d2654c0fde833ca80c8db20845eba147
nihil0000 <nihil.dev0@gmail.com>
2025-03-09 17:55:30 +0900

    fix: Typo seeder name

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(ProductsTableSeeder::class);
-        $this->call(SeasonsTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
+        $this->call(SeasonTableSeeder::class);
     }
 }
