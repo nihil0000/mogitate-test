37bf081f20761fe730446d9f22109545d16093c4
nihil0000 <nihil.dev0@gmail.com>
2025-03-09 14:21:57 +0900

    feat: Add layout and products

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/app')
+
+@section('css')
+
+@section('content')
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+
+    <title>mogitate</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <p class="header__title">mogitate</p>
+        </div>
+    </header>
+
+    <main class="main__container">
+        @yield('content')
+    </main>
+</body>
+
+</html>
