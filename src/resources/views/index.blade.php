72779684e388a62525d64694fc5474891666fe83
nihil0000 <nihil.dev0@gmail.com>
2025-03-09 18:01:33 +0900

    feat: Add index (with controller and css)

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts/app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
 
 @section('content')
+<div class="container">
+    <div class="title__wrapper">
+        <p class="title">商品一覧</p>
+        <button class="btn-add-product">+ 商品を追加</button>
+    </div>
+
+    <section class="product-list-wrapper">
+        <div class="search__wrapper">
+            <input type="text" class="input-search">
+            <button class="btn-search">検索</button>
+
+            <p class="title-sort">価格順で表示</p>
+            <select name="" id="" class="sort-price">
+                <option value=""></option>
+            </select>
+        </div>
+
+
+        <div class="product-list">
+            @foreach ($products as $product)
+            <div class="product__card">
+                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="product-img">
+                <div class="product__info">
+                    <p class="product__name">{{ $product->name }}</p>
+                    <p class="product__price">¥{{ $product->price }}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </section>
+</div>
+@endsection
+
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function () {
-    return view('index');
+// Route::get('/products', function () {
+//     return view('index');
+// });
+
+Route::controller(ProductController::class)->prefix('products')->name('products.')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('index'); // Product List
 });
