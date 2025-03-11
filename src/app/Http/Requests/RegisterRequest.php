d879289b6ac55450344fb02cca92e4a5240a23dd
nihil0000 <nihil.dev0@gmail.com>
2025-03-11 10:50:38 +0900

    fix: Changed varidation rule

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
             'name' => 'required',
             'price' => 'required|numeric|min:0|max:10000',
             'seasons' => 'required',
-            'image' => 'required|mimes:png, jpeg',
+            'image' => 'required|mimes:png,jpeg',
             'description' => 'required|max:120',
         ];
     }
